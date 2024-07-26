b41c2ea9c7ad9c812b178ea2e435528d3e565a8e
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-07-26 12:19:06 +0530

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,12 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 5.0"  # Specify the version for the AWS provider
+        }
+    }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
