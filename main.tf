2a80e99713638d4a5229ef3ed085a1295e6ff843
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-07-26 12:18:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+terraform {
+    required_version = ">= 1.0.0"  # Specify the required version
+}
+
 resource "aws_vpc" "example_vpc" {
   cidr_block       = var.cidr_block
   instance_tenancy = var.instance_tenancy
